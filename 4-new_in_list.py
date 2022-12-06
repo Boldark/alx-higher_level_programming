70cfa2ef0896e527540df919e96e6ba5317bf92c
Boldark <apaokagiashraf@gmail.com>
2022-12-06 10:41:40 +0100

    Rest of the tasks

---
+++ 4-new_in_list.py
@@ -0,0 +1,12 @@
+#1/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= my_list:
+        retutn ("None")
+    else:
+        copied_list = copy_list(my_list)
+        copied_list[idx] = element
+        return (my_list)
+
+def copy_list(my_list):
+    copied_list = my_list
+    return copied_list
---
+++ 5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    remove_c = ""
+    for x in my_string:
+        if x == 'c' and x =='C':
+            remoce_c -= x
+        return (remove_c)
