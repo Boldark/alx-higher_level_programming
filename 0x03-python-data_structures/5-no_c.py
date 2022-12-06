6405d615f7e6226686cc2ac7d2b3844cd091298d
Boldark <apaokagiashraf@gmail.com>
2022-12-06 11:57:06 +0100

    Task 5 Hot fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,7 +5,3 @@ def no_c(my_string):
         if x != 'c' and x != 'C':
             remove_c += x
     return (remove_c)
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
